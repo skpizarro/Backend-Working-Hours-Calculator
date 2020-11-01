050c81adccd0f57bef503d39518fe794d5b00f10
Juan Pablo Pizarro <juanpablopizarro97@gmail.com>
2020-11-01 09:59:48 -0500

    Cálculo de horas extras terminadas

--- src/controllers/calWorkHours.controllers.js
+++ src/controllers/calWorkHours.controllers.js
@@ -38,7 +38,11 @@ const calculate = (services,weekNumber)=>{
     sundayExtraH=0,
     auxNormal=0,
     auxNight=0,
-    auxSun=0
+    auxSun=0,
+    flag = true,
+    maxH = 48;
+
+
 
 
     services.forEach(s=>{
@@ -47,27 +51,36 @@ const calculate = (services,weekNumber)=>{
 
         // hallamos la cantidad de dias
         
+        // si la hora de inicio y hora fin están en la misma semana
         if(DateTime.fromJSDate(startS).weekNumber == weekNumber && DateTime.fromJSDate(endS).weekNumber == weekNumber){
             let resp;
             // condición para las horas semanasles
             if(weekH < 48){ // se calculan las horas normales
-                resp = calculateWorkingH(startS,endS,normalH,nightH,sundayH,auxNormal,auxNight,auxSun,weekH)
-                weekH = resp.normalH + resp.nightH + resp.sundayH
+        
+                resp = calculateWorkingH(startS,endS,normalH,nightH,sundayH,auxNormal,auxNight,auxSun,weekH,flag,maxH)
+                weekH = resp.weekH;
                 normalH = resp.normalH;
                 nightH = resp.nightH;
-                sundayH = resp.sundayH; 
+                sundayH = resp.sundayH;
+                normalExtraH = resp.auxNormal;
+                nightExtraH= resp.auxNight;
+                sundayExtraH = resp.auxSun;
+                
+
             }else{
+                flag = false
                 console.log("Extraaas")
-                // Se calculan las horas extras
-                resp = calculateWorkingH(startS,endS,normalExtraH,nightExtraH,sundayExtraH,auxNormal,auxNight,auxSun,weekH)
-                weekH = resp.normalH + resp.nightH + resp.sundayH + normalH + nightH + sundayH
+                // Se calculan solo las horas extras
+                resp = calculateWorkingH(startS,endS,normalExtraH,nightExtraH,sundayExtraH,auxNormal,auxNight,auxSun,weekH,flag,maxH)
+                weekH = resp.weekH;
                 normalExtraH = resp.normalH;
                 nightExtraH = resp.nightH;
                 sundayExtraH = resp.sundayH;
             }
 
         }
-        else if(DateTime.fromJSDate(startS).weekNumber == weekNumber && DateTime.fromJSDate(endS).weekNumber > weekNumber){
+        else if(DateTime.fromJSDate(startS).weekNumber == weekNumber && DateTime.fromJSDate(endS).weekNumber > weekNumber){ // creo que nunca se mete REVISAR
+            console.log("si inicia domingo y finaliza lunes de otra semana")
             if(weekH < 48){
                 sundayH = sundayH + (24-startS.getHours()); // No tenemos en cuenta las horas de fin si termina el lunes
                 weekH = weekH + (24-startS.getHours())
@@ -91,7 +104,7 @@ const calculate = (services,weekNumber)=>{
 }
 
 
-const calculateWorkingH = (startS,endS,normalH,nightH,sundayH,auxNormal,auxNight,auxSun,weekH)=>{
+const calculateWorkingH = (startS,endS,normalH,nightH,sundayH,auxNormal,auxNight,auxSun,weekH,flag,maxH)=>{
     // Definimos horarios
     let normI = new Date('10-05-2020 07:00:00'),
      normF= new Date('10-05-2020 20:00:00'),
@@ -122,13 +135,62 @@ const calculateWorkingH = (startS,endS,normalH,nightH,sundayH,auxNormal,auxNight
     let days=(endS-startS)/(1000* 60* 60* 24); // dias que trabajo
 
 
+
+
     // Funciones para  las horas extras
-    const extraType1 =()=>{
-        
+
+    // se asigna las horas extra si solo hay un turno (norma o nocturno)
+    const extraType1 =(weekH,aux,maxH,h)=>{
+        // si las horas semanales > 48 en este caso
+        if(weekH > maxH){
+            aux = weekH - maxH // obtenemos las extras
+            console.log("aux ",aux)
+            h = h - aux // le resto las extras a las normales
+            //extras = extras + aux; // aumeta horas extra
+            console.log("h ",h)
+            return{
+                h,
+                aux,
+                //extras,
+            }
+        }
+        return{
+            h,
+            aux,
+            //extras,
+        }
+
     }
 
-    const extraType2=()=>
-    {}
+    // se usa cuando pasamos de normal a noctuno o al contrario. para asignar las horas extra donde correspondan
+    const extraType2=(weekH,aux,aux2,maxH,h)=>
+    {
+        if((weekH > maxH) && aux == 0){
+            aux2 = weekH - maxH // obtenemos las extras
+            h = h - aux2 // le resto las extras a las normales
+            //extras = extras + aux2; // aumeta horas extra
+
+            return{
+                h,
+                aux2,
+                //extras,
+           }
+        }else if((weekH > maxH) && aux > 0){
+            //solo se tienen en cuenta las extras
+            aux2 = weekH - aux - maxH; // las semanas menos las extras anteriores menos el tope(48)
+            return{
+                h,
+                aux2
+            }
+        }
+
+        // los devuelve como estaban
+        return{
+            h,
+            aux2
+        }
+    }
+
 
 
     //let hours = ms / (1000 * 60 * 60);
@@ -138,6 +200,15 @@ const calculateWorkingH = (startS,endS,normalH,nightH,sundayH,auxNormal,auxNight
         if((startH >= normI && startH < normF) && (endH > normI && endH <= normF)){ /// 7AM a 8 PM
             console.log("Horario normal  7AM a 8 PM")
             normalH=normalH + hours;
+            weekH = weekH + hours; // aumentamos las horas semanales
+            
+            
+            if(flag){// significa que las semanas no habian llegado a 48 pero pudieron llegarlo en esta iteracion, entoces se confirma y si es positivo se efectua la operación de horas extras
+                let resp = extraType1(weekH,auxNormal,maxH,normalH)
+                auxNormal = resp.aux;
+                normalH = resp.h;
+            }
+            
         
         }else if((startH >= normI && startH < normF) && endH >= nightI){ // // empezó hora normal y termino en nocturna del mismo dia (como atienden emergencias, se puede dar el caso)
             
@@ -146,8 +217,24 @@ const calculateWorkingH = (startS,endS,normalH,nightH,sundayH,auxNormal,auxNight
 
             // separamos las horas normales de las nocturnas
             normalH = normalH + (nightI - startH)
+            weekH = weekH + (nightI - startH);
+
+            if(flag){
+                let resp = extraType1(weekH,auxNormal,maxH,normalH)
+                auxNormal = resp.aux;
+                normalH = resp.h;
+            }
+
             //Normal Nocturnas
-            nightH= nightH + (endH - nightI)
+            nightH= nightH + (endH - nightI);
+            weekH = weekH + (endH - nightI);
+
+            if(flag){
+                let resp = extraType2(weekH,auxNormal,auxNight,maxH,nightH);
+                auxNight = resp.aux2;
+                nightH = resp.h;
+            }
+            
         }
         else if((startH >= normI && startH < normF) && endH <= nightF){ // empezó hora hormal y terminó hora nocturna del dia siguiente de la misma semana
             console.log("7AM a 8PM + 8pm a 12am + 12am a 7am")
@@ -155,8 +242,23 @@ const calculateWorkingH = (startS,endS,normalH,nightH,sundayH,auxNormal,auxNight
 
             // separamos las horas normales de las nocturnas
             normalH = normalH + (nightI - startH) // normales
+            weekH = weekH + (nightI - startH);
+            
+            if(flag){
+                let resp = extraType1(weekH,auxNormal,maxH,normalH)
+                auxNormal = resp.aux;
+                normalH = resp.h;
+            }
+
             //Normal Nocturnas
-            nightH= nightH + (4 + endH) // se suman las primeras nocturnas (8pm hasta las 12pm) y de 12am hasta las 7am
+            nightH= nightH + (4 + endH) // se suman las primeras nocturnas (8pm hasta las 12pm) y de 12am hasta las 7am;
+            weekH = weekH + (4 + endH)
+
+            if(flag){
+                let resp = extraType2(weekH,auxNormal,auxNight,maxH,nightH);
+                auxNight = resp.aux2;
+                nightH = resp.h;
+            }
         }
 
         else if((startH >= nightI && endH > nightI) || (startH >= nightI && endH == 0)){ // Hora nocturna
@@ -164,53 +266,151 @@ const calculateWorkingH = (startS,endS,normalH,nightH,sundayH,auxNormal,auxNight
             console.log("8pm a 12am")
             
             if(hours<0){
-                nightH = nightH + (24+hours)
+                nightH = nightH + (24+hours);
+                weekH = weekH + (24+hours)
+
+                if(flag){
+                    let resp = extraType1(weekH,auxNight,maxH,nightH)
+                    auxNight = resp.aux;
+                    nightH = resp.h;
+                }
+
             }else{
-                nightH = nightH + hours
+                nightH = nightH + hours;
+                weekH = weekH + hours;
+                if(flag){
+                    let resp = extraType1(weekH,auxNight,maxH,nightH)
+                    auxNight = resp.aux;
+                    nightH = resp.h;
+                }
             } 
         
         }else if(startH >= nightI && endH <= nightF ){
             console.log("de 8pm a 7 am");
             // de 8pm a 7am
             nightH = nightH + (24 - startH) +(endH) // de 8pm a 12am + de 12am a 7am
+            weekH = weekH + (24 - startH) +(endH)
+            if(flag){
+                let resp = extraType1(weekH,auxNight,maxH,nightH)
+                auxNight = resp.aux;
+                nightH = resp.h;
+            }
         }
 
         else if(startH >= nightI && (endH >= normI && endH <= nightI )){
             // de 8pm a 8pm del dia sig
             console.log("de 8pm a 8pm del dia sig")
             nightH = nightH + (24 - startH) + 7 // horas nocturnas del dia anterior mas todas del siguiente dia
+            weekH = weekH + (24 - startH) + 7
+
+            if(flag){
+                let resp = extraType1(weekH,auxNight,maxH,nightH)
+                auxNight = resp.aux;
+                nightH = resp.h;
+            }
+
             normalH = normalH + (endH - 7) // las horas normales del dia siguiente
+            weekH = weekH + (endH - 7);
+            if(flag){
+                let resp = extraType2(weekH,auxNight,auxNormal,maxH,normalH);
+                auxNormal = resp.aux2;
+                normalH = resp.h;
+            }
+            
         }
         else if((startH >= 0 && startH <= normI) && (endH > 0 && endH <= normI)){ // inició y termino de 12 am a 7 am del mismo dia
             //Nocturnas de 12am a 7am
             console.log("de 12am a 7am")
             nightH = nightH + hours;
+            weekH = weekH + hours;
+            if(flag){
+                let resp = extraType1(weekH,auxNight,maxH,nightH)
+                auxNight = resp.aux;
+                nightH = resp.h;
+            }
         }
         else if((startH >= 0 && startH <= normI) && (endH > normI && endH <= normF)){ //inició de 12 am y terminó al dia siguiente en hora normal
             console.log("12 am a 8pm")
             // nocturnas
-            nightH = nightH + (nightF - startH)
+            nightH = nightH + (nightF - startH);
+            weekH = weekH + (nightF - startH)
+
+            if(flag){
+                let resp = extraType1(weekH,auxNight,maxH,nightH)
+                auxNight = resp.aux;
+                nightH = resp.h;
+            }
+
             // normales
             normalH = normalH + (endH-normI);
+            weekH = weekH + (endH-normI);
+            if(flag){
+                let resp = extraType2(weekH,auxNight,auxNormal,maxH,normalH);
+                auxNormal = resp.aux2;
+                normalH = resp.h;
+            }
+            
         }
         else if((startH >= 0 && startH <= normI) && (endH >=nightI || endH == 0 )){
             console.log("12am a 12am del sig dia")
             nightH = nightH + (nightF - startH);// nocturnas de 12am hasta 7am
+            weekH = weekH + (nightF - startH);
+
+            if(flag){
+                let resp = extraType1(weekH,auxNight,maxH,nightH)
+                auxNight = resp.aux;
+                nightH = resp.h;
+            }
+
             normalH = normalH + (nightI - normI); // 7am a 8pm
+            weekH = weekH + (nightI - normI);
+
+            if(flag){
+                let resp = extraType2(weekH,auxNight,auxNormal,maxH,normalH);
+                auxNormal = resp.aux2;
+                normalH = resp.h;
+            }
             
             // las nocturnas de 8pm a 12am
             if(endH == 0){
                 nightH = nightH + 4
+                weekH = weekH + 4
+
+                if(flag){
+                    let resp = extraType1(weekH,auxNight,maxH,nightH)
+                    auxNight = resp.aux;
+                    nightH = resp.h;
+                }
+
             }else{
                 nightH = nightH + (endH-nightI);
+                weekH = weekH + (endH-nightI);
+                if(flag){
+                    let resp = extraType1(weekH,auxNight,maxH,nightH)
+                    auxNight = resp.aux;
+                    nightH = resp.h;
+                }
             }
             
         }
 
     }else{
-        //Domingos cuando la hora fin no finaliza ,(misma semana)
+        //Domingos cuando la hora fin finaliza ,(misma semana)
         console.log("Domingos")
+        console.log("hours ",hours);
         sundayH= sundayH + hours;
+        console.log("sundayH ",sundayH);
+        console.log("week ",weekH)
+        weekH = weekH + hours;
+        console.log("week ",weekH);
+
+        if(flag){
+            let resp = extraType1(weekH,auxSun,maxH,sundayH)
+            auxSun = resp.aux;
+            sundayH = resp.h;
+            console.log("auxSun ",auxSun);
+            console.log("sundayH ",sundayH);
+        }
     }
 
     if(days >= 1){
@@ -219,12 +419,13 @@ const calculateWorkingH = (startS,endS,normalH,nightH,sundayH,auxNormal,auxNight
     }
 
     return {
+        weekH,
         normalH,
         nightH,
         sundayH,
         auxNormal,
         auxNight,
-        auxSun
+        auxSun,
     }
     
 
